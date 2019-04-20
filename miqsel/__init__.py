8e6804b1d51fed3aa211ee40aa45b22023dc24fa
Nikhil Dhandre <nik.digitronik@live.com>
2019-04-20 14:40:43 +0530

    adding server commands
    Signed-off-by: Nikhil Dhandre <nik.digitronik@live.com>

--- miqsel/__init__.py
+++ miqsel/__init__.py
@@ -1,6 +1,7 @@
 import click
 
 from miqsel.config import config
+from miqsel.server import start, status, stop, urls, viewer
 
 
 @click.version_option()
@@ -12,6 +13,13 @@ def main():
 
 # config command
 main.add_command(config)
+
+# server management command
+main.add_command(status)
+main.add_command(start)
+main.add_command(stop)
+main.add_command(urls)
+main.add_command(viewer)
 
 
 if __name__ == "__main__":
