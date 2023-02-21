0eab114f90ab056c1fa8b3a1ecd993b93edd9d05
Nikhil Dhandre <ndhandre@redhat.com>
2023-02-21 16:25:14 +0530

    Fix stale containers

--- miqsel/server.py
+++ miqsel/server.py
@@ -43,6 +43,11 @@ class SeleniumContainer(object):
     def is_running(self):
         cmd = subprocess.run([self.client, "ps"], stdout=subprocess.PIPE)
         return self.name in cmd.stdout.decode()
+    
+    @property
+    def is_stale(self):
+        cmd = subprocess.run([self.client, "ps", "-a"], stdout=subprocess.PIPE)
+        return self.name in cmd.stdout.decode() and not self.is_running
 
     @property
     def executor(self):
@@ -64,6 +69,10 @@ class SeleniumContainer(object):
 
     def start(self, **kwargs):
         """Start selenium container"""
+        if self.is_stale:
+            cmd = [self.client, "rm", self.name]
+            subprocess.run(cmd)
+
         if not self.is_running:
             cmd = [
                 self.client,
